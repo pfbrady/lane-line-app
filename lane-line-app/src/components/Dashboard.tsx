081074893bb299545059db571ae9ac57b31218af
pfbrady <50747698+pfbrady@users.noreply.github.com>
2021-09-30 16:53:26 -0400

    Creatred basic dashboard

---
+++ lane-line-app/src/components/Dashboard.tsx
@@ -0,0 +1,11 @@
+import { Col, Row, Card, Button } from "react-bootstrap";
+
+export function Dashboard(): JSX.Element {
+  return (
+    <Card>
+      <Row></Row>
+      <Row></Row>
+      <Row></Row>
+    </Card>
+  );
+}
