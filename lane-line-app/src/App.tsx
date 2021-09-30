d706a4ce0a94d8c1e8d33e2b901b93e76b55bb1f
PatrickB <patrick16brady@gmail.com>
2021-09-30 12:17:25 -0400

    Added testing pool and lane

--- lane-line-app/src/App.tsx
+++ lane-line-app/src/App.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import LANES from './assets/poolDefault.json';
+import { LaneViewer } from './components/LaneViewer';
+import { Lane } from './interfaces/lane';
+import { PoolViewer } from './components/PoolViewer';
+import { Pool } from './interfaces/pool';
 
 function App() {
+  const [lane, setLane] = useState<Lane>(LANES[0] as Lane);
+  const [pool, setPool] = useState<Pool>({lanes: LANES, numberOfLanes: 10} as Pool);
+
+
   return (
     <div className="App">
       <header className="App-header">
-
       </header>
+    <PoolViewer pool={pool} setPool={setPool} setLane={setLane}></PoolViewer>
+    <LaneViewer lane={lane} setLane={setLane}></LaneViewer>
     </div>
   );
 }
