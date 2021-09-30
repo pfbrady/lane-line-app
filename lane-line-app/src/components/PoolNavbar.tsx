604e6777acd1027d241de8e3e7f435967be225b6
pfbrady <50747698+pfbrady@users.noreply.github.com>
2021-09-30 15:35:00 -0400

    basic navbar created

---
+++ lane-line-app/src/components/PoolNavbar.tsx
@@ -0,0 +1,25 @@
+import { Container, Navbar, Nav } from "react-bootstrap";
+
+export function PoolNavbar(): JSX.Element {
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img
+            alt=""
+            src="/logo.svg"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{" "}
+          React Bootstrap
+        </Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+}
