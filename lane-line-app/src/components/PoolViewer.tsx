29699aca961d4370777e8bea2878788e870899e1
pfbrady <50747698+pfbrady@users.noreply.github.com>
2021-09-30 22:09:44 -0400

    hard coded the pool JSX return :( maps are hard

--- lane-line-app/src/components/PoolViewer.tsx
+++ lane-line-app/src/components/PoolViewer.tsx
@@ -1,18 +1,19 @@
+import { useState } from "react";
 import { Pool } from "../interfaces/pool";
 import { Lane } from "../interfaces/lane";
 import { Col, Row, Card } from "react-bootstrap";
 import { LaneViewer } from "./LaneViewer";
-import { LanguageServiceMode, PollingWatchKind } from "typescript";
+import LANES from "../assets/poolDefault.json";
 
 export function PoolViewer({
   pool,
   setPool,
-  setLane,
 }: {
   pool: Pool;
   setPool: (p: Pool) => void;
-  setLane: (l: Lane) => void;
 }): JSX.Element {
+  const [lane, setLane] = useState<Lane>(LANES[0] as Lane);
+
   /**
    * Takes the date, calculates the milliseconds since the new year, and
    * determines if it is summer pool season or not (i.e. if the 10 lane outdoor
@@ -22,8 +23,7 @@ export function PoolViewer({
    */
   function isSummer(): boolean {
     let todayDate: Date = new Date();
-    let todayDateNum: number =
-      todayDate.getTime() - new Date(todayDate.getFullYear(), 0, 1).getTime();
+    let todayDateNum: number = todayDate.getTime();
 
     if (
       new Date(todayDate.getFullYear(), 4, 24).getTime() <= todayDateNum &&
@@ -35,16 +35,50 @@ export function PoolViewer({
     }
   }
 
-  if (isSummer() && pool.numberOfLanes === 8) {
-    pool.lanes.push();
-    pool.lanes.push();
-    pool.numberOfLanes = pool.lanes.length;
+  if (isSummer()) {
+    if (pool.numberOfLanes === 8) {
+      pool.lanes.push();
+      pool.lanes.push();
+      pool.numberOfLanes = pool.lanes.length;
+    }
+    return (
+      <Card>
+        <LaneViewer lane={pool.lanes[0]} setLane={setLane}></LaneViewer>
+        <LaneViewer lane={pool.lanes[1]} setLane={setLane}></LaneViewer>
+        <LaneViewer lane={pool.lanes[2]} setLane={setLane}></LaneViewer>
+        <LaneViewer lane={pool.lanes[3]} setLane={setLane}></LaneViewer>
+        <LaneViewer lane={pool.lanes[4]} setLane={setLane}></LaneViewer>
+        <LaneViewer lane={pool.lanes[5]} setLane={setLane}></LaneViewer>
+        <LaneViewer lane={pool.lanes[6]} setLane={setLane}></LaneViewer>
+        <LaneViewer lane={pool.lanes[7]} setLane={setLane}></LaneViewer>
+      </Card>
+    );
   } else if (!isSummer() && pool.numberOfLanes === 10) {
-    pool.lanes.pop();
-    pool.lanes.pop();
-    pool.numberOfLanes = pool.lanes.length;
+    if (pool.numberOfLanes === 10) {
+      pool.lanes.pop();
+      pool.lanes.pop();
+      pool.numberOfLanes = pool.lanes.length;
+    }
+    return (
+      <Card>
+        <LaneViewer lane={pool.lanes[0]} setLane={setLane}></LaneViewer>
+        <LaneViewer lane={pool.lanes[1]} setLane={setLane}></LaneViewer>
+        <LaneViewer lane={pool.lanes[2]} setLane={setLane}></LaneViewer>
+        <LaneViewer lane={pool.lanes[3]} setLane={setLane}></LaneViewer>
+        <LaneViewer lane={pool.lanes[4]} setLane={setLane}></LaneViewer>
+        <LaneViewer lane={pool.lanes[5]} setLane={setLane}></LaneViewer>
+        <LaneViewer lane={pool.lanes[6]} setLane={setLane}></LaneViewer>
+        <LaneViewer lane={pool.lanes[7]} setLane={setLane}></LaneViewer>
+        <LaneViewer lane={pool.lanes[8]} setLane={setLane}></LaneViewer>
+        <LaneViewer lane={pool.lanes[9]} setLane={setLane}></LaneViewer>
+      </Card>
+    );
+  } else {
+    console.log(pool.numberOfLanes);
+    return <div>[Pool Render]</div>;
   }
-  pool.lanes.map(
+  /** 
+   *   pool.lanes.map(
     (lane: Lane, laneIndex: number): JSX.Element => (
       <LaneViewer
         lane={pool.lanes[laneIndex - 1]}
@@ -52,5 +86,9 @@ export function PoolViewer({
       ></LaneViewer>
     )
   );
-  return <div></div>;
+  */
+
+  /** after many long hours trying to use maps to dynamically render the pool,
+   * I've decided to cop out and hard code it
+   */
 }
